efef98d48b22d678ca0e6c314d011eb6f0ac24da
jdragon <jdraong9527@gmail.com>
2024-06-07 17:12:30 +0800

    feat: KafkaConnectors

---
+++ src/main/java/org/simple/flink/connectors/KafkaConnectors.java
@@ -0,0 +1,29 @@
+package org.simple.flink.connectors;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class KafkaConnectors {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        KafkaSource<String> source = KafkaSource.<String>builder()
+                .setBootstrapServers("vmhost:9092")
+                .setTopics("quickstart-events")
+                .setGroupId("my-group")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
+
+        DataStreamSource<String> kafkaSource = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+
+        kafkaSource.print("kafka:");
+
+        env.execute();
+    }
+}
